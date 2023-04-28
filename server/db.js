86f122cc18e4f6c170bf081eb30688455f3b9f47
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-28 20:18:19 +0300

    Create a variable ENV for the database

--- server/db.js
+++ server/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 export async function connectDB(){
     try {
-        await mongoose.connect("mongodb+srv://seher23:parola23@cluster0.x1kw7ly.mongodb.net/mongodb?retryWrites=true&w=majority")
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("Database is connected");
         
     } catch (error) {
