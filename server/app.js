2b8dbab3ef533f60cc45ba1621a8295c5e6571f4
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-29 17:29:17 +0300

    Add the cors module for fetch data

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import post_routes from "./routes/posts_routes.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors())
 
 // middlewares
 app.use(express.json());
