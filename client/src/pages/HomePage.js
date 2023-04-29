e691b33d90e65ae2c174c23f5c787cb6cb53e67a
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-29 17:29:50 +0300

    Fetch the data from database and print in console

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,19 @@
 
+import { useState, useEffect } from "react";
+
 const HomePage = () => {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch("http://localhost:4000/posts");
+            const data = await res.json()
+
+            console.log(data);
+        }
+
+        fetchData()
+    }, [])
+
     return(
         <div>
             <h2>Home page</h2>
