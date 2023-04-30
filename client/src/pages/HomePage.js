3ff78a84b3c72509dabd8077911463fea46572f0
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-30 10:50:08 +0300

    client: Create the filter users on the interface

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,20 @@
 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
+
 
 const HomePage = () => {
 
+    const [ users, setUsers ] = useState([]);
+    const [ query, setQuery ] = useState()
+
     useEffect(() => {
         const fetchData = async () => {
-            const res = await fetch("http://localhost:4000/posts");
+            const res = await fetch("http://localhost:4000/register");
             const data = await res.json()
 
             console.log(data);
+            setUsers(data)
         }
 
         fetchData()
@@ -19,6 +24,20 @@ const HomePage = () => {
         <div>
             <NavBar />
             <h2>Home page</h2>
+            <input
+                onChange={(e) => setQuery(e.target.value)}
+                type="text" 
+                name="search" 
+                placeholder="Search user"
+            />
+
+            {users.filter((user) => user.first_name.includes(query)).map((user, index) => (
+                <div key={index}>
+                    <h2>{user.first_name}</h2>
+                    <p>{user.last_name}</p>
+                    <p>{user.age}</p>
+                </div>
+            ))}
         </div>
     )
 }
