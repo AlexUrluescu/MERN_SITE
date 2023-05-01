f257ca10ecb976611bb9c434ed56e4cfcb218483
AlexUrluescu <urluescualex2003@gmail.com>
2023-05-01 14:05:07 +0300

    client-login.js: Fetch the url login from react

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,52 @@
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import NavBar from "../components/NavBar";
 import "../css/Login.css";
 
+const initialUser = {
+    email: "",
+    password: ""
+}
+
 const Login = () => {
+
+    const [user, setUser] = useState(initialUser);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setUser((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        console.log(user);
+
+        const res = await fetch("http://localhost:4000/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+        });
+
+        const data = await res.json();
+        console.log(data);
+
+        if(data.status === "ok"){
+            console.log('Login succesful');
+            window.localStorage.setItem("token", data.data);
+            window.location.href = "./UserData";
+        }
+
+    }
+
     return(
         <div>
             <NavBar />
@@ -13,9 +56,20 @@ const Login = () => {
                         <h1><b>Welcome in Web School</b></h1>
                         <h2>Login with your email and password</h2>
                     </div>
-                    <form className="login_form">
-                        <input className="input_login" type="text" placeholder="Email" />
-                        <input className="input_login" type="password" placeholder="Password" />
+                    <form onSubmit={handleSubmit} className="login_form">
+                        <input
+                            onChange={handleChange}
+                            className="input_login" 
+                            type="text" 
+                            placeholder="Email"
+                            name="email" />
+                
+                        <input
+                            onChange={handleChange}
+                            className="input_login" 
+                            type="password" 
+                            placeholder="Password"
+                            name="password" />
 
                         <input className="login_button" type="submit" value="Login"/>
                     </form>
