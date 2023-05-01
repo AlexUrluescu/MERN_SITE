b85dcb15ae8e52264c62899cd9dfbc128042a71a
AlexUrluescu <urluescualex2003@gmail.com>
2023-05-01 14:07:52 +0300

    client: a space

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,6 +3,7 @@ import Create from "./Create";
 import NotFoundPage from "./NotFoundPage";
 import Register from "./Register";
 import Login from "./Login";
+// import UserData from "./UserData";
 
 export {
     HomePage,
